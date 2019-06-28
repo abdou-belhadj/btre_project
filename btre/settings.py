7fc565c6bed0c6991dc8c662ffaa18742242d9fd
abderrahman <abderrahmanbelhadj0gmail.com>
2019-06-28 03:06:48 +0100

    remove email

--- btre/settings.py
+++ btre/settings.py
@@ -165,18 +165,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# Email config
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER='abderrahmanbelhadj@gmail.com'
-# EMAIL_HOST_PASSWORD='Gmail0071996'
-# EMAIL_USE_TLS=True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'bthouses96@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bthouses1996007'
-EMAIL_PORT = 587
+
 
 
 try:
